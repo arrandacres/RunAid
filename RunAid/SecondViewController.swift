af935e79507665665f0f1fc859164aaf0033d6fc
Arran <arran.dacres@gmail.com>
2018-02-21 17:59:11 +0000

    Initial Project Creation

---
+++ RunAid/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RunAid
+//
+//  Created by Arran Dacres on 21/02/2018.
+//  Copyright © 2018 Arran Dacres. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
